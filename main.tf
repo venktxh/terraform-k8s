f1ea6c6b472501976d4fec26134a91af9899ed7e
venkatesh-13 <venkeysanikommu@gmail.com>
2026-03-17 18:04:30 +0530

    initial terraform project

---
+++ main.tf
@@ -0,0 +1,104 @@
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+resource "kubernetes_namespace" "ns" {
+  metadata {
+    name = "btech-project"
+  }
+}
+
+# NGINX Deployment
+resource "kubernetes_deployment" "nginx" {
+  metadata {
+    name      = "nginx-deployment"
+    namespace = "btech-project"
+  }
+
+  spec {
+    replicas = 2
+
+    selector {
+      match_labels = {
+        app = "nginx"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "nginx"
+        }
+      }
+
+      spec {
+        container {
+          name  = "nginx"
+          image = "nginx:latest"
+
+          port {
+            container_port = 80
+          }
+        }
+      }
+    }
+  }
+}
+
+# MongoDB Deployment
+resource "kubernetes_deployment" "mongodb" {
+  metadata {
+    name      = "mongodb"
+    namespace = "btech-project"
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "mongodb"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "mongodb"
+        }
+      }
+
+      spec {
+        container {
+          name  = "mongodb"
+          image = "mongo:latest"
+
+          port {
+            container_port = 27017
+          }
+        }
+      }
+    }
+  }
+}
+
+# NGINX Service
+resource "kubernetes_service" "nginx_service" {
+  metadata {
+    name      = "nginx-service"
+    namespace = "btech-project"
+  }
+
+  spec {
+    selector = {
+      app = "nginx"
+    }
+
+    port {
+      port        = 80
+      target_port = 80
+    }
+
+    type = "NodePort"
+  }
+}
